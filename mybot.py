5be9fe5df24808b27da3cff5025dfb3dc3a90ed1
TheFridayProger <44724333+TheFridayProger@users.noreply.github.com>
2018-11-15 20:25:16 +0500

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -1,2 +1,19 @@
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+import time
+
+
+vk = vk_api.VkApi(login = '87773830616', password = 'RE2122')
+vk.auth()
+longpoll = VkLongPoll(vk)
+
+
 while True:
-  print('ok')
+    try:
+        for event in longpoll.listen():
+            if (event.type == VkEventType.MESSAGE_NEW):
+                if (event.to_me):
+                    vk.method('messages.send', {'peer_id': event.peer_id, 'message': event.text})
+    except:
+        print('Ошибочка. Я АФК на 30 сек.')
+        time.sleep(30)
